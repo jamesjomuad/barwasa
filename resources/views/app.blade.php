d75f8ce4eb71f50ef1380f1deafccd6ddb2bfff9
James Jomuad <jjomuad@realresponse.com.au>
2023-11-04 09:50:40 +0800

    Update head title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome to Quasar</title>
+    <title>Barwsa</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <link href="{{ asset('/build/css/app.css') }}" rel="stylesheet" type="text/css">
