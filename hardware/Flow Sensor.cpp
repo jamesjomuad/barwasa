27c9209b564c669c1f894f4311222d3910043687
James Jomuad <jjomuad@realresponse.com.au>
2023-11-11 14:35:28 +0800

    Hardwares

---
+++ hardware/Flow Sensor.cpp
@@ -0,0 +1,25 @@
+int sensorPin = D2;
+volatile long pulse;
+unsigned long lastTime;
+float volume;
+
+void setup() {
+    pinMode(sensorPin, INPUT);
+    Serial.begin(9600);
+    attachInterrupt(digitalPinToInterrupt(sensorPin), increase, RISING);
+}
+
+void loop() {
+    volume = 2.663 * pulse / 1000 * 30;
+    if (millis() - lastTime > 2000) {
+        pulse = 0;
+        lastTime = millis();
+    }
+    Serial.print(pulse);
+    Serial.println(" L/m");
+    delay(1000);
+}
+
+ICACHE_RAM_ATTR void increase() {
+    pulse++;
+}
